4ddcc6062db5c0619aa414d2adfb2c9a57681414
melouie <me-louie@users.noreply.github.com>
2021-10-11 23:24:05 -0700

    Minor variable name formating change

--- scheduler/src/ast/SchedulerVisitor.java
+++ scheduler/src/ast/SchedulerVisitor.java
@@ -14,6 +14,6 @@ public interface SchedulerVisitor<T> {
     T visit(Apply a) throws ProgramValidationException;
     T visit(Merge m) throws ProgramValidationException;
     T visit(Loop l) throws ProgramValidationException;
-    T visit(IfThenElse ifThenElse) throws ProgramValidationException;
-    T visit(Cond cond) throws ProgramValidationException;
+    T visit(IfThenElse i) throws ProgramValidationException;
+    T visit(Cond c) throws ProgramValidationException;
 }
