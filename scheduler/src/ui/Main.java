08e238cb337c81bf01f21aa670256a82debb31c7
Usamah <usamah13@students.cs.ubc.ca>
2021-10-14 16:38:44 -0700

    master mergre done

--- scheduler/src/ui/Main.java
+++ scheduler/src/ui/Main.java
@@ -20,7 +20,7 @@ import validate.ResultNotFound;
 
 public class Main {
     public static void main(String[] args) throws IOException, ProgramValidationException {
-        SchedulerLexer lexer = new SchedulerLexer(CharStreams.fromFileName("ExampleLoop.txt"));
+        SchedulerLexer lexer = new SchedulerLexer(CharStreams.fromFileName("Example3.txt"));
 
         for (Token token : lexer.getAllTokens()) {
             System.out.println(token);
